11a50043e65275c2739ac919aee98b24808c2f9f
mikepadiernos <mike.padiernos@gmail.com>
2020-07-31 23:44:52 -0400

    [FEATURE] passed / names.py
    - finished code for names.py
    - added binary_search_tree.py
    - passed all tests

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BSTNode
 
 start_time = time.time()
 
@@ -14,50 +15,18 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 
 # Replace the nested for loops below with your improvements
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
-
-
-class BSTNode:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-    # Insert the given value into the tree
-    def insert(self, value):
-        if value < self.value:
-            if not self.left:
-                self.left = BSTNode(value)
-            else:
-                self.left.insert(value)
-        elif value >= self.value:
-            if not self.right:
-                self.right = BSTNode(value)
-            else:
-                self.right.insert(value)
-
-    def contains(self, name):
-        if self.value == name:
-            return True
-        if self.value < name:
-            if self.right:
-                return self.right.contains(name)
-            else:
-                return False
-        if self.value > name:
-            if self.left:
-                return self.left.contains(name)
-            else:
-                return False
-
+# for name in names_1:
+#     cur = BSTNode(name)
+#     for name in names_1:
+#         for name_2 in names_2:
+#             if name_1 == name_2:
+#                 if cur.contains(name):
+#                     duplicates.append(name)
 
 for name in names_1:
     cur = BSTNode(name)
-
-for name in names_1:
+    named = name
+# for name in names_1:
     cur.insert(name)
 
 for name in names_2:
