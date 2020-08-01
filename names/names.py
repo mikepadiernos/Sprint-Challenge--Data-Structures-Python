87d93f6a67737a28fce907bd02188d580bdf22db
mikepadiernos <mike.padiernos@gmail.com>
2020-07-31 23:09:41 -0400

    [FEATURE] passed / names.py
    - finished code for names.py
    - passed all tests

--- names/names.py
+++ names/names.py
@@ -12,11 +12,57 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, name):
+        if self.value == name:
+            return True
+        if self.value < name:
+            if self.right:
+                return self.right.contains(name)
+            else:
+                return False
+        if self.value > name:
+            if self.left:
+                return self.left.contains(name)
+            else:
+                return False
+
+
+for name in names_1:
+    cur = BSTNode(name)
+
+for name in names_1:
+    cur.insert(name)
+
+for name in names_2:
+    if cur.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
